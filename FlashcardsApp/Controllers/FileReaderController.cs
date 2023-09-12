5837ebc417af592d88fb17e70911ddd869ff5adb
Emilis <emiliskleinas2003@gmail.com>
2023-09-13 00:26:02 +0300

    Added basic file upload funcionality

---
+++ FlashcardsApp/Controllers/FileReaderController.cs
@@ -0,0 +1,40 @@
+﻿using FlashcardsApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FlashcardsApp.Controllers
+{
+    public class FileReaderController : Controller
+    {
+		private FileData _data = new FileData() { };
+		public IActionResult Index()
+		{
+            return View(_data);
+		}
+		
+        [HttpPost]
+        public IActionResult Index(List<IFormFile> files)
+        {
+            long size = files.Sum(f => f.Length);
+
+            var filePaths = new List<string>();
+            foreach (var formFile in files)
+            {
+                if (formFile.Length > 0)
+                {
+                    var filePath = Path.GetTempFileName();
+                    filePaths.Add(filePath);
+                    using (var stream = new FileStream(filePath, FileMode.Create))
+                    {
+                        formFile.CopyTo(stream);
+                    }
+                }
+            }
+            _data.FileName = filePaths[0];
+            _data.Text = new StreamReader(filePaths[0]).ReadToEnd();
+            _data.FileSize = _data.Text.Length;
+
+            return View(_data);
+
+        }
+    }
+}
---
+++ FlashcardsApp/Models/FileData.cs
@@ -0,0 +1,9 @@
+﻿namespace FlashcardsApp.Models
+{
+    public class FileData
+    {
+        public string FileName { get; set; }
+        public int FileSize { get; set; }
+        public string Text { get; set; }
+    }
+}
