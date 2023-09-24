7b5da626b3bbaf69f8384c8e3a0a3c5b3bfc51f2
Gintare Keraite <gintare.keraite18@gmail.com>
2023-09-24 19:52:12 +0300

    Fixed flashcards displaying bug, added error message if front or back of the flashcard is not filled

--- FlashcardsApp/Controllers/FlashcardCollectionController.cs
+++ FlashcardsApp/Controllers/FlashcardCollectionController.cs
@@ -54,49 +54,43 @@ namespace FlashcardsApp.Controllers
         [HttpPost]
         public IActionResult Edit(FlashcardCollection collection, string NewFlashcardFrontSide, string NewFlashcardBackSide)
         {
-            if (ModelState.IsValid)
+            var existingCollection = flashcardCollections.FirstOrDefault(c => c.CollectionId == collection.CollectionId);
+
+            // Check if the collection exists
+            if (existingCollection == null)
             {
-                if (!string.IsNullOrEmpty(NewFlashcardFrontSide) && !string.IsNullOrEmpty(NewFlashcardBackSide))
-                {
-                    var newFlashcard = new FlashcardViewModel
-                    {
-                        Question = NewFlashcardFrontSide,
-                        Answer = NewFlashcardBackSide
-                    };
-
-                    if (collection.Flashcards == null)
-                    {
-                        collection.Flashcards = new List<FlashcardViewModel>();
-                    }
-
-                    collection.Flashcards.Add(newFlashcard);
-                }
-
-                var existingCollection = flashcardCollections.FirstOrDefault(c => c.CollectionId == collection.CollectionId);
-                if (existingCollection != null)
-                {
-                    existingCollection.CollectionName = collection.CollectionName;
-                    existingCollection.FlashcardsAppUserId = collection.FlashcardsAppUserId;
-
-                    if (existingCollection.Flashcards == null)
-                    {
-                        existingCollection.Flashcards = new List<FlashcardViewModel>();
-                    }
-
-                    foreach (var flashcard in collection.Flashcards)
-                    {
-                        existingCollection.Flashcards.Add(flashcard);
-                    }
-                }
-                else
-                {
-                    return NotFound("Collection not found");
-                }
-
-                return RedirectToAction("Edit", new { id = collection.CollectionId });
+                return NotFound("Collection not found");
             }
 
+            // Validate the new flashcard data
+            if (string.IsNullOrEmpty(NewFlashcardFrontSide) || string.IsNullOrEmpty(NewFlashcardBackSide))
+            {
+                ModelState.AddModelError("NewFlashcardError", "Both Front Side and Back Side must be filled out.");
+            }
+            else
+            {
+                // Add the new flashcard to the existing collection
+                var newFlashcard = new FlashcardViewModel
+                {
+                    Question = NewFlashcardFrontSide,
+                    Answer = NewFlashcardBackSide
+                };
+
+                existingCollection.Flashcards ??= new List<FlashcardViewModel>();
+
+                existingCollection.Flashcards.Add(newFlashcard);
+
+                // Redirect to the Edit view for the updated collection
+                return RedirectToAction("Edit", new { id = existingCollection.CollectionId });
+            }
+
+            // If we've reached this point, it means validation failed. 
+            // Reset the Flashcards to the existing ones before returning the View.
+            collection.Flashcards = existingCollection.Flashcards;
             return View(collection);
         }
+
+
+
     }
 }
