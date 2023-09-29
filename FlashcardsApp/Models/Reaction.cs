f43084f4d1b1abf07f53a52f4f5cb9ab943595aa
Tadas <118689974+Tadas37@users.noreply.github.com>
2023-09-29 18:18:31 +0300

    Update Reaction.cs

--- FlashcardsApp/Models/Reaction.cs
+++ FlashcardsApp/Models/Reaction.cs
@@ -2,7 +2,9 @@
 {
     public class Reaction
     {
+        [Key]
         public int ReactionId { get; set; }
+        
         public ReactionType Type { get; set; }
         public int Count { get; set; }
 
