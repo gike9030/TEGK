26edf49a9c394b7c687e60cbcac8504d1f3f96e7
Gintare Keraite <gintare.keraite18@gmail.com>
2023-11-13 13:14:17 +0200

    comments fix

--- FlashcardsAPI/Controllers/CommentsController.cs
+++ FlashcardsAPI/Controllers/CommentsController.cs
@@ -21,19 +21,7 @@ namespace FlashcardsAPI.Controllers
             _context = context ?? throw new ArgumentNullException(nameof(context));
         }
 
-        // GET: api/Comments
-        [HttpGet]
-        [ProducesResponseType(StatusCodes.Status200OK)]
-        [ProducesResponseType(StatusCodes.Status404NotFound)]
-        public async Task<ActionResult<IEnumerable<Comment>>> GetComments()
-        {
-            if (_context.Comments == null)
-            {
-                return NotFound("Comments collection is not available.");
-            }
-            return await _context.Comments.ToListAsync();
-        }
-
+    
         // GET: api/Comments/5
         [HttpGet("{id}")]
         [ProducesResponseType(StatusCodes.Status200OK)]
@@ -55,27 +43,7 @@ namespace FlashcardsAPI.Controllers
             return comment;
         }
 
-        // POST: api/Comments
-        [HttpPost]
-        [ProducesResponseType(StatusCodes.Status201Created)]
-        [ProducesResponseType(StatusCodes.Status400BadRequest)]
-        public async Task<ActionResult<Comment>> PostComment([FromBody] Comment comment)
-        {
-            if (_context.Comments == null)
-            {
-                return Problem("Entity set 'ApplicationDbContext.Comments' is null.");
-            }
 
-            if (!ModelState.IsValid)
-            {
-                return BadRequest(ModelState);
-            }
-
-            _context.Comments.Add(comment);
-            await _context.SaveChangesAsync();
-
-            return CreatedAtAction(nameof(GetComment), new { id = comment.Id }, comment);
-        }
 
         // PUT: api/Comments/5
         [HttpPut("{id}")]
