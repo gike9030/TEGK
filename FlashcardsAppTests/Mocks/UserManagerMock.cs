cbe0eddcb84f8ac835fd352388f919df5ff28add
Tadas37 <slavinskastadas37@gmail.com>
2023-12-05 00:53:45 +0200

    Added tests for CommentsController

---
+++ FlashcardsAppTests/Mocks/UserManagerMock.cs
@@ -0,0 +1,29 @@
+﻿using FlashcardsApp.Areas.Identity.Data;
+using Microsoft.AspNetCore.Identity;
+using Moq;
+using System;
+using System.Collections.Generic;
+
+public static class UserManagerMock
+{
+    public static Mock<UserManager<FlashcardsAppUser>> CreateMockUserManager()
+    {
+        var store = new Mock<IUserStore<FlashcardsAppUser>>();
+        var userManager = new Mock<UserManager<FlashcardsAppUser>>(
+            store.Object,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null);
+
+        userManager.Setup(um => um.GetUserAsync(It.IsAny<System.Security.Claims.ClaimsPrincipal>()))
+                           .ReturnsAsync(new FlashcardsAppUser { });
+
+        return userManager;
+    }
+}
+
